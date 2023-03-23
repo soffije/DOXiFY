f934deb047f79a655328b939f770e03f18db0729
Sofiia Kireieva <sofiiakireieva@gmail.com>
2023-03-23 12:05:44 +0200

    Implemented chat events.

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,6 +1,7 @@
 import React, { createContext, useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import Web3 from 'web3'
+import { w3cwebsocket as W3CWebSocket } from 'websocket';
 
 import ChatBar from './ChatBar/ChatBar'
 import ChatBody from './ChatBody/ChatBody'
@@ -11,20 +12,57 @@ import { subscribeUser } from '../../features/chat/chatSlice'
 import { getUserAddress } from '../../features/user/userSlice'
 export const ChatContext = createContext()
 
+const WEBSOCKET_URL = 'wss://api.avax-test.network/ext/bc/C/ws'; // URL веб-сокета Avalanche Fuji Testnet
+
 function Chat() {
   const dispatch = useDispatch()
   const address = useSelector(getUserAddress)
 
-  const web3 = new Web3('https://api.avax-test.network/ext/bc/C/rpc')
-  const contract = new web3.eth.Contract(abi, contractAddress)
-  const [chatInstances] = useState({
-    web3: web3,
-    contract: contract,
-  })
+  const [chatInstances, setChatInstances] = useState(null);
 
   useEffect(() => {
-    dispatch(subscribeUser({ web3, contract, address }))
-  }, [address])
+    const web3 = new Web3(new Web3.providers.WebsocketProvider(WEBSOCKET_URL));
+    const contract = new web3.eth.Contract(abi, contractAddress);
+
+    setChatInstances({
+      web3: web3,
+      contract: contract,
+    });
+
+    return () => {
+      web3.currentProvider.disconnect();
+    }
+  }, []);
+
+  useEffect(() => {
+    if (chatInstances) {
+      const { web3, contract } = chatInstances;
+
+      const client = new W3CWebSocket(WEBSOCKET_URL);
+      client.onopen = () => {
+        console.log('WebSocket Client Connected');
+
+        contract.events.MessageSent({})
+          .on('data', (event) => {
+            console.log('Event data:', event.returnValues);
+            dispatch(subscribeUser({ web3, contract, address }));
+          })
+          .on('error', console.error);
+      };
+
+      client.onclose = () => {
+        console.log('WebSocket Client Disconnected');
+      };
+
+      return () => {
+        client.close();
+      }
+    }
+  }, [chatInstances, dispatch, address]);
+
+  if (!chatInstances) {
+    return null;
+  }
 
   return (
     <div className="row chat-container shadow">
@@ -37,3 +75,6 @@ function Chat() {
 }
 
 export default Chat
+
+
+
