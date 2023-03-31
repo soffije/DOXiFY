059053b27ae710496822c82b88056748ff5a159f
Sataff <93443681+Sataff@users.noreply.github.com>
2023-03-31 16:51:37 +0300

    Fixed indexDB component.

--- src/api/indexDB.js
+++ src/api/indexDB.js
@@ -2,7 +2,7 @@ const DB_NAME = 'my-database'
 const DB_VERSION = 1
 const STORE_NAME = 'users'
 
-export async function getUser(userAddress) {
+export async function getUser(user) {
   return new Promise((resolve, reject) => {
     const request = indexedDB.open(DB_NAME, DB_VERSION)
 
@@ -16,7 +16,7 @@ export async function getUser(userAddress) {
       const tx = db.transaction(STORE_NAME, 'readonly')
       const store = tx.objectStore(STORE_NAME)
 
-      const requestGet = store.get(userAddress)
+      const requestGet = store.get(user.address)
 
       requestGet.onsuccess = function () {
         const user = requestGet.result
@@ -50,55 +50,7 @@ export async function getUser(userAddress) {
     }
   })
 }
-
-export async function addUser(userAddress) {
-  return new Promise((resolve, reject) => {
-    const request = indexedDB.open(DB_NAME, DB_VERSION)
-
-    request.onupgradeneeded = function (event) {
-      const upgradeDb = event.target.result
-      upgradeDb.createObjectStore(STORE_NAME, { keyPath: 'address' })
-    }
-
-    request.onsuccess = function () {
-      const db = request.result
-      const tx = db.transaction(STORE_NAME, 'readwrite')
-      const store = tx.objectStore(STORE_NAME)
-
-      const requestPut = store.put(userAddress)
-
-      requestPut.onsuccess = function () {
-        resolve()
-      }
-
-      requestPut.onerror = function (event) {
-        console.error(
-          'Error adding/updating user in IndexedDB',
-          event.target.error
-        )
-        reject(event.target.error)
-      }
-
-      tx.onerror = function (event) {
-        console.error(
-          'Error adding/updating user in IndexedDB',
-          event.target.error
-        )
-        reject(event.target.error)
-      }
-
-      tx.oncomplete = function () {
-        db.close()
-      }
-    }
-
-    request.onerror = function (event) {
-      console.error('Error opening IndexedDB database', event.target.error)
-      reject(event.target.error)
-    }
-  })
-}
-export async function deleteUser(userAddress) {
+export async function adduser(user) {
   return new Promise((resolve, reject) => {
     const request = indexedDB.open(DB_NAME, DB_VERSION)
 
@@ -112,7 +64,46 @@ export async function deleteUser(userAddress) {
       const tx = db.transaction(STORE_NAME, 'readwrite')
       const store = tx.objectStore(STORE_NAME)
 
-      const requestDelete = store.delete(userAddress)
+      const requestAdd = store.add(user, user.address)
+
+      requestAdd.onsuccess = function () {
+        resolve()
+      }
+
+      requestAdd.onerror = function (event) {
+        console.error('Error adding friend to IndexedDB', event.target.error)
+        reject(event.target.error)
+      }
+
+      tx.onerror = function (event) {
+        console.error('Error adding friend to IndexedDB', event.target.error)
+        reject(event.target.error)
+      }
+
+      db.close()
+    }
+
+    request.onerror = function (event) {
+      console.error('Error opening IndexedDB database', event.target.error)
+      reject(event.target.error)
+    }
+  })
+}
+export async function deleteUser(user) {
+  return new Promise((resolve, reject) => {
+    const request = indexedDB.open(DB_NAME, DB_VERSION)
+
+    request.onupgradeneeded = function (event) {
+      const upgradeDb = event.target.result
+      upgradeDb.createObjectStore(STORE_NAME)
+    }
+
+    request.onsuccess = function () {
+      const db = request.result
+      const tx = db.transaction(STORE_NAME, 'readwrite')
+      const store = tx.objectStore(STORE_NAME)
+
+      const requestDelete = store.delete(user.address)
 
       requestDelete.onsuccess = function () {
         resolve()
