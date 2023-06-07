df0aeaa3d17fb7018ef7873dc7818c4507eecea8
Sataff <93443681+Sataff@users.noreply.github.com>
2023-06-07 12:52:39 +0300

    Little fixes in toast component.

--- src/components/ToastComponent/ToastComponent.jsx
+++ src/components/ToastComponent/ToastComponent.jsx
@@ -1,24 +1,33 @@
 import React from 'react'
 import { Toast } from 'react-bootstrap'
-
+import ToastContainer from 'react-bootstrap/ToastContainer'
 import './Toast.css'
 
 function ToastComponent({ show, setShow, setMessage, message, variant }) {
   const toastClasses = `Toast ${variant}`
   return (
-    <Toast
-      className={toastClasses}
-      onClose={() => (setShow(false), setMessage(message))}
-      show={show}
-      delay={3000}
-      autohide
-    >
-      <Toast.Header>
-        <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
-        <strong className="me-auto">Notification</strong>
-      </Toast.Header>
-      <Toast.Body>{message}</Toast.Body>
-    </Toast>
+    <div aria-live="polite" aria-atomic="true">
+      <ToastContainer position="top-end" className="p-3">
+        <Toast
+          className={toastClasses}
+          onClose={() => (setShow(false), setMessage(message))}
+          show={show}
+          delay={8000}
+          autohide
+        >
+          <Toast.Header>
+            <img
+              src="holder.js/20x20?text=%20"
+              className="rounded me-2"
+              alt=""
+            />
+            <strong className="me-auto">Notification</strong>
+          </Toast.Header>
+
+          <Toast.Body>{message}</Toast.Body>
+        </Toast>
+      </ToastContainer>
+    </div>
   )
 }
 
